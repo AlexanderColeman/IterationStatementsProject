5ac6fb38730ab8ea0f4286e3fecaecf78a69e8c0
Alexander Coleman <alexanderjcoleman01@gmail.com>
2022-07-08 11:15:52 -0500

    first commit

---
+++ IterationStatements/MethodsWithLoopsAndConditionals.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IterationStatements
+{
+    public class MethodsWithLoopsAndConditionals
+    {
+
+        public void NegativeToPositive()
+        {
+            for (int i = -1000; i <= 1000; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public void By3()
+        {
+            for (int i = 3; i <= 999; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public void isEqual(int x, int y)
+        {
+            if (x == y)
+            {
+                Console.WriteLine($"{x} is equal to {y}");
+            }
+            else
+            {
+                Console.WriteLine($"{x} is not equal to {y}");
+            }
+        }
+
+        public void IsEvenOrOdd()
+        {
+            Console.Write("Enter a number to see if it is even or odd ");
+            var number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"{number} is even");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is odd");
+            }
+        }
+
+        public void IsPostiveOrNegative()
+        {
+            Console.Write("Enter a number to see if it is postive or negative ");
+            var number = int.Parse(Console.ReadLine());
+
+            if (number > 0)
+            {
+                Console.WriteLine($"{number} is postive");
+
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine($"{number} is negative");
+            }
+            else
+            {
+                Console.WriteLine($"Your number is {number} neither postive or negative");
+            }
+
+
+        }
+
+        public void VotingAgeCheck()
+        {
+            Console.Write("Enter your age and see if you can vote ");
+            var userAge = int.Parse(Console.ReadLine());
+
+            if(userAge >= 0 && userAge < 18)
+            {
+                Console.WriteLine("You are not old enough to vote");
+            }
+            else if (userAge >= 18 && userAge <= 125)
+            {
+                Console.WriteLine("You are old enough to vote");
+            }
+            else
+            {
+                Console.WriteLine("Not a valid age");
+            }
+        }
+
+        public void IsInRange()
+        {
+
+            Console.Write("Enter a number to see if it is in range of -10 to 10 ");
+            var userNumber = int.Parse(Console.ReadLine());
+
+            if(userNumber >= -10 && userNumber <= 10)
+            {
+                Console.WriteLine($"{userNumber} is within range");
+            }
+            else
+            {
+                Console.WriteLine($"{userNumber} is not within range");
+            }
+
+        }
+
+        public void MultiplicationTable()
+        {
+            Console.Write("Enter a number from 1 to 12 to see its multiplication table ");
+            var userNumber = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 1; i <= 12; i++)
+            {
+                sum = userNumber * i;
+                Console.WriteLine($"{userNumber} * {i} is: {sum}");
+            }
+        }
+
+
+    }
+}
--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -7,66 +7,38 @@ namespace IterationStatements
     {
         static void Main(string[] args)
         {
-                //TODO - Read each comment and complete its instruction
-                // like the example below
+            //Exercises.Exercise1();
 
-                //Create a List called "numbers" - DONE!
-                var numbers = new List<int>();
-            
-            //-----START HERE------------------------------------------
-            //Create a variable of type int and name it num
-            //initialize the variable with a value of 0
+            //Exercises.Exercise2();
 
+            var exercise3 = new MethodsWithLoopsAndConditionals();
 
+            //exercise3.NegativeToPositive();
 
-            // Create a do-while loop and use the template below:
+            //exercise3.By3();
 
-            {
-                // Increment num by 1
+            //exercise3.isEqual(1456, 43);
 
-                // Then add num to the collection - numbers
-                    // Hint: reference num inside of the Add method's parentheses
-                numbers.Add(/* num */);
+            //exercise3.IsEvenOrOdd();
 
-            } // <---- While your variable is less than 100
+            //exercise3.IsPostiveOrNegative();
 
+            //exercise3.VotingAgeCheck();
+
+            //exercise3.IsInRange();
+
+            //exercise3.MultiplicationTable();
 
 
-            // Create a while loop
-            // <--- While num is less than 200
-            {
-                // Increment num by 1
-                // Then add num to the collection numbers
-                    //HINT: copy how this was done in the do while loop
-
-            }
 
 
-            // This is to show the user that the numbers will start increasing on the console
-            Console.WriteLine("Increase:");
 
 
-            // Create a foreach loop using the collection - numbers
-                //In the scope of the foreach loop, print each number in numbers
-             
 
 
-            Console.WriteLine("");
-            Console.WriteLine("Decrease:");
 
 
-            // Create a for loop - this will print the numbers in reverse order - from 200 to 1
-                // in your initializer set the value of i to 199
-                // in your conditional, as long as i is less than or equal to the amount of items in "numbers" - use (numbers.Count)
-                // AND as long as i is greater than or equal to 0
-                // Decrement i by 1
 
-            //start for loop here
-            {
-                // place numbers[i] inside of the Console.WriteLine() method
-            }
-
-            //------------End of exercise
         }
     }
 }
